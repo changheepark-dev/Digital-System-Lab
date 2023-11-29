09702869c8168bed2452f1eadc55668c9d738272
changheepark-dev <pchee0504@gmail.com>
2023-11-30 04:37:06 +0900

    11-30-2023

--- GccApplication3/main.c
+++ GccApplication3/main.c
@@ -5,97 +5,115 @@
 #include "lcd.h"				//LCD 사용을 위한 헤더 include -> " "외부의 파일을 include
 
 // setting ///////////////////////////////////////////////////////////////////////////////
-unsigned int measurement_time = 15624 * ( 0.2 );				// 0 ~ 4 [s]
-unsigned char time_average[( 29 )] = {0};
-unsigned char average_count = ( 29 );							// 배렬 선언과 동일한 값
-unsigned char threshold = ( 29 ) * ( 0.8 );						// 0 ~ 1 역치값
+// time average
+#define measurement_time ( 0.2 )							// 0.1 ~ 4 [s]
+unsigned char time_average[( 10 )] = {0};
+#define average_count ( 10 )								// 배렬 선언과 동일한 값
+
+// sensor
+#define TRIG 7												// PE7 (변경 불가)
+#define TRIG_delay 80										// 기본 80, 연산 속도에 맞춰 변경 가능, 너무 줄일 시 측정 오류
+#define interval 700										// 측정 간격 5 ~ 2700 [mm]
 
 
 unsigned char count = 0;
-unsigned char average[7] = {0};
+unsigned char average[8] = {0};
+unsigned char av = 0;
+unsigned char measurement;
+
+unsigned char check = 0;
+
+
 ISR(TIMER1_COMPA_vect) {
-	for (unsigned char av = 0; av < 8; av++) {
-		if (0 != (time_average[count] & (0x01 << av))) {
-			average[av] --;
+	
+	unsigned char temp = time_average[count];
+	time_average[count] = check;
+	for (av = 0; av < 8; av++) {
+		if (((temp ^ time_average[count]) >> av) & 0x01) {
+			if ((temp >> av) & 0x01) {
+				average[av] --;
+			}
+			else {
+				average[av] ++;
+			}
 		}
 	}
 	
-	time_average[count] = PINB;
-	for (unsigned char av = 0; av < 8; av++) {
-		if (0 != (time_average[count] & (0x01 << av))) {
-			average[av] ++;
-		}
-	}
-
 	count ++;
-	if (count == average_count + 1) {
-	count = 0;
+	if (count == average_count) {
+		count = 0;
 	}
+	
+	measurement = 1;
 }
+
 void measurement_time_Init() {						// 초음파 계측 주기 : // 1초
 	TCCR1A = (0<<WGM11) | (0<<WGM10);
 	TCCR1B = (0<<WGM13) | (1<<WGM12) | (1<<CS12) | (0<<CS11) | (1<<CS10);
 	TIMSK |= (1 << OCIE1A);
-	OCR1A = measurement_time;
+	OCR1A = 15624 * measurement_time;
 	sei();
 }
 
-
-
-
-
-
-
-int LCD_nNum(unsigned int NUM, unsigned int NUM2) {
-	unsigned char Buff[13] = "0";
-	Buff[0] = '0'+((NUM %10000)/1000);
-	Buff[1] = '0'+((NUM %1000)/100);
-	Buff[2] = '0'+((NUM %100)/10);
-	Buff[3] = '0'+(NUM %10);
-	Buff[4] = 's';
-	
-	Buff[5] = ' ';
-	Buff[6] = '0'+(((NUM2) %1000)/100);
-	Buff[7] = '0'+(((NUM2) %100)/10);
-	Buff[8] = '0'+((NUM2) %10);
-	Buff[9] = 'm';
-	Buff[10] = 's';
-	
-	LCDPuts(Buff);
-}
 int tNum(unsigned int NUM) {
-	unsigned char Buff[6] = "0";
-	Buff[0] = '0'+((NUM %100000)/10000);
-	Buff[1] = '0'+((NUM %10000)/1000);
-	Buff[2] = '0'+((NUM %1000)/100);
-	Buff[3] = '0'+((NUM %100)/10);
-	Buff[4] = '0'+(NUM %10);
+	unsigned char Buff[4] = "0";
+	Buff[0] = '0'+((NUM %1000)/100);
+	Buff[1] = '0'+((NUM %100)/10);
+	Buff[2] = '0'+(NUM %10);
 	LCDPuts(Buff);
 }
 
 
-unsigned char pp = 0;
-int s = 0;
+
 int main(void){
-	DDRB |= 0xf0;				//7~4 LED 3~0 스위치가 연결되어 있다. 상위 니블 7~4까지는 출력을로 설정 하위 니블 3~0 입력으로 설정
-	DDRG |= (1<<PG3);			//PG3번에 buzzer 연결되어있음
-	UDR0 |= 0x00;
+	DDRE=0x80;
+	PORTE = 0x7f;
 
 	measurement_time_Init();
 	MCU_Init();					//LCD 사용을 위한 MCU 설정
 	LCDInit();					//LCD 사용을 위한 초기화 설정
-		
+	
 	while(1){
-			LCDMove(0,0);
-			tNum(PINB);
-			LCDMove(1,0);
-			tNum(average[0]);
-			LCDMove(1,7);
-			tNum(average[7]);
+		if (measurement == 1) {
+			check = 0;
+			TCCR0 = 7;
+			
+			PORTE |= (1<<TRIG);     //Trig=HIGH -> 거리 측정 명령 시작
+			_delay_us(10);			//10us동안 유지
+			PORTE &= ~(1<<TRIG);
+			
+			while (!(PINE & (1<<0))) {}
+			TCNT0 = 0;
+			
+			while (TCNT0 < ((interval + 50) / 10.88)){
+				for (unsigned char a = 0 ; a < 7; a++) {
+					if (PINE & (1<<a)) {
+						if (TCNT0 > ((interval) / 10.88)) {
+							check |= (1 << a);
+						}
+					}
+				}
+			}
+			TCCR0 = 0;
+			measurement = 0;
+		}
+		
+		LCDMove(0,0);
+		tNum(average[0]);
+		LCDMove(0,4);
+		tNum(average[1]);
+		LCDMove(0,8);
+		tNum(average[2]);
+		LCDMove(0,12);
+		tNum(average[3]);
+		
+		LCDMove(1,0);
+		tNum(average[4]);
+		LCDMove(1,4);
+		tNum(average[5]);
+		LCDMove(1,8);
+		tNum(average[6]);
+		LCDMove(1,12);
+		tNum(check);
 	}
 }
-
-
-
-
-//unsigned char
