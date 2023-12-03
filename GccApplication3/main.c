7f1dbf78879a8d5767bf6fbef9d63334e7b1fc81
changheepark-dev <pchee0504@gmail.com>
2023-12-03 20:08:31 +0900

    12-3-2023 1차완료

--- GccApplication3/main.c
+++ GccApplication3/main.c
@@ -1,5 +1,6 @@
 #define F_CPU 16000000UL		// 16MHz의 오실레이터를 사용
 #include <avr/io.h>				//입출력 포트 사용을 위한 헤더 include
+#define __DELAY_BACKWARD_COMPATIBLE__
 #include <avr/delay.h>			//딜레이 사용을 위한 헤더 inlcude
 #include <avr/interrupt.h>		//인터럽트 사용을 위한 헤더 include
 #include "lcd.h"				//LCD 사용을 위한 헤더 include -> " "외부의 파일을 include
@@ -11,25 +12,23 @@ unsigned char time_average[10];								// 시간 평균 갯수
 #define average_count 10									// 배렬 선언과 동일한 값
 
 // sensor
-#define TRIG 7												// PE7 (변경 불가)
+#define TRIG 3												// PE0 (변경 불가), echo(pe1 ~ 3, pd4 ~ 7)
 #define TRIG_delay 80										// 기본 80 [ms], 연산 속도에 맞춰 변경 가능, 너무 줄일 시 측정 오류
 #define interval 700										// 측정 간격 5 ~ 2700 [mm]
 
 //measurement
 #define standard 50											// 0 ~ 100[%], 측정 민감도 (낮을수록 늦게 상승, 빨리 감소)
-#define night_period
-#define daytime_period
-
+#define night_period 20										// 0 ~ 65535 [s] 전면 센서 밤에 on 시간
+#define daytime_period 30									// 0 ~ 65535 [s] 전면 센서 낮에 on 시간
+#define person 10											// 0 ~ 65535 [s] 동행자 인지 판단 시간
 ///////////////////////////////////////////////////////////////////////////////
-
 // time average
 unsigned char count = 0;
 unsigned char average[7] = {0};
-unsigned char av = 0;
+unsigned char av = 0; 
 unsigned char measurement;
 // sensor
 unsigned char check = 0;
-
 ISR(TIMER1_COMPA_vect) {
 	unsigned char temp = time_average[count];
 	time_average[count] = check;
@@ -58,7 +57,7 @@ void measurement_time_Init() {						// 초음파 계측 주기 타이머
 	sei();
 }
 
-int h = 12, m = 30, s = 30;
+unsigned char h = 12, m = 30, s = 30, l;
 unsigned int front[7] = {0};
 ISR(TIMER3_COMPA_vect) {
 	// 시계
@@ -81,11 +80,59 @@ void time_Init() {									// 시간 타이머
 	TCCR3A = (0<<WGM11) | (0<<WGM10);
 	TCCR3B = (0<<WGM13) | (1<<WGM12) | (1<<CS12) | (0<<CS11) | (1<<CS10);
 	ETIMSK |= (1 << OCIE3A);
-	OCR3A = 15624;
+	OCR3A = 5000;//15624;
 	sei();
 }
 
-int tNum(unsigned int NUM) {
+
+// 후면 감지
+char ckd = 0;
+ISR(INT0_vect) {
+	if (ckd == 0) {ckd = 5;}
+}
+ISR(INT1_vect) {
+	if (ckd == 0) {ckd = 6;}
+}
+ISR(INT2_vect) {
+	if (ckd == 0) {ckd = 3;} // 사용 x
+}
+ISR(INT3_vect) {
+	if (ckd == 0) {ckd = 7;}
+}
+ISR(INT4_vect) {
+	if (ckd == 0) {ckd = 1;}
+}
+ISR(INT5_vect) {
+	if (ckd == 0) {ckd = 2;}
+}
+ISR(INT6_vect) {
+	if (ckd == 0) {ckd = 3;}
+}
+ISR(INT7_vect) {
+	if (ckd == 0) {ckd = 4;}
+}
+
+void INTR_Init() {
+	DDRD = 0x00;
+	PORTD = 0xff;
+	
+	EICRA = 0xff;		//하강엣지 트리거
+	EICRB = 0xff;
+	EIMSK = 0xff;
+	
+	DDRE = 0x08;
+	PORTE = 0xf7;
+	sei();
+}
+
+
+int tNum2(unsigned int NUM) {
+	unsigned char Buff[3] = "0";
+	Buff[0] = '0'+((NUM %100)/10);
+	Buff[1] = '0'+(NUM %10);
+	LCDPuts(Buff);
+}
+int tNum3(unsigned int NUM) {
 	unsigned char Buff[4] = "0";
 	Buff[0] = '0'+((NUM %1000)/100);
 	Buff[1] = '0'+((NUM %100)/10);
@@ -111,66 +158,91 @@ int Watch(unsigned int y) {
 	LCDPuts(wc);
 }
 
-
-
+int bz(unsigned int a, unsigned int b) {
+	PORTG |= (1<<PG3);
+	_delay_ms(a);
+	PORTG &= ~(1<<PG3);
+	_delay_ms(b);
+}
 
 int main(void){
-	DDRB=0xf0;
-	DDRE=0x80;
-	PORTE = 0x7f;
-	UDR0 |= 0x00;
-	
+	DDRB = 0xf0;
+	DDRG |= (1<<PG3);
 	MCU_Init();
 	LCDInit();
 	
 	// 시간 설정
+	static char string1[]="set location";
 	static char string8[]="set Watch       ";
-	unsigned char next = 4;
-	while (next > 0) {
+	
+	unsigned char next = 5;
+	while (1) {
 		// 화면
-		LCDMove(0,0);
-		if (next > 1) {
-			for (char a = 12 ; a < 15 ; a++) {string8[a] = ' ';}
-			if (next == 4) {string8[15] = 'h';}
-			else if (next == 3) {string8[15] = 'm';}
-			else {string8[15] = 's';}
+		if (next == 5) {
+			LCDMove(0,0);
+			LCDPuts(string1);
+			LCDMove(1,0);
+			tNum3(l);
 		}
 		else {
-			string8[12] = 'n';
-			string8[13] = 'e';
-			string8[14] = 'x';
-			string8[15] = 't';
+			LCDMove(0,0);
+			if (next > 1) {
+				for (char a = 12 ; a < 15 ; a++) {string8[a] = ' ';}
+				if (next == 4) {string8[15] = 'h';}
+				else if (next == 3) {string8[15] = 'm';}
+				else {string8[15] = 's';}
+			}
+			else {
+				string8[12] = 'n';
+				string8[13] = 'e';
+				string8[14] = 'x';
+				string8[15] = 't';
+			}
+			LCDPuts(string8);
+			Watch(1);
 		}
-		LCDPuts(string8);
-		Watch(1);
 		
 		// 버튼
 		if (PINB == 14) {
+			if (next == 5 && l != 0) {l--;}
 			if (next == 4 && h != 0) {h--;}
 			if (next == 3 && m != 0) {m--;}
 			if (next == 2 && s != 0) {s--;}
 			_delay_ms(200);
 		}
 		if (PINB == 13) {
+			if (next == 5 && l != 255) {l++;}
 			if (next == 4 && h != 23) {h++;}
 			if (next == 3 && m != 59) {m++;}
 			if (next == 2 && s != 59) {s++;}
 			_delay_ms(200);
 		}
-		if (PINB == 11 && next != 4) {
+		if (PINB == 11 && next != 5) {
+			LCDInit();
 			next ++;
 			_delay_ms(200);
 		}
 		if (PINB == 7) {
 			next --;
+			if(next == 0){
+				break;
+			}
 			_delay_ms(200);
 		}
 	}
 	time_Init();
 	measurement_time_Init();
+	INTR_Init();
 	LCDInit();
 	
+	static char string2[]="location : 000  ";
+	string2[11] = '0'+((l %1000)/100);
+	string2[12] = '0'+((l %100)/10);
+	string2[13] = '0'+(l %10);
+	ckd = 0;
+	
 	// main
+	unsigned char disp = 0;
 	while(1){
 		// 초음파 측정
 		if (measurement == 1) {
@@ -185,10 +257,17 @@ int main(void){
 			TCNT0 = 0;
 			
 			while (TCNT0 < ((interval + 50) / 10.88)){
-				for (unsigned char a = 0 ; a < 7; a++) {
+				for (char a = 0 ; a < 3; a++) {
 					if (PINE & (1<<a)) {
 						if (TCNT0 > ((interval) / 10.88)) {
 							check |= (1 << a);
+						}
+					}
+				}
+				for (char a = 4 ; a < 8; a++) {
+					if (PIND & (1<<a)) {
+						if (TCNT0 > ((interval) / 10.88)) {
+							check |= (1 << a - 1);
 						}
 					}
 				}
@@ -196,36 +275,132 @@ int main(void){
 			TCCR0 = 0;
 			measurement = 0;
 		}
-
+		
+		// 전면 센서 작동
+		int largest = 1, secondLargest = 1;
+		for (char a = 0; a < 7 ; a++) {
+			if (front[a] >= largest) {
+				secondLargest = largest;
+				largest = front[a];
+			}
+			else if (front[a] > secondLargest && front[a] < largest) {
+				secondLargest = front[a];
+			}
+		}
+		if ((h >= 19) || (h < 7)) {
+			if ((largest > night_period) && secondLargest < person) {
+				PORTB = 0xf0;
+			}
+			else {PORTB = 0x00;}
+		}
+		else {
+			if ((largest > daytime_period) && secondLargest < person) {
+				PORTB = 0xf0;
+			}
+			else {PORTB = 0x00;}
+		}
+				
+		// 후면 센서 작동
+		while (ckd != 0 && disp == 14) {				
+			string2[14] = '-';
+			string2[15] = '0'+(ckd %10);
+			LCDMove(0,0);
+			LCDPuts(string2);
+			
+			for (char a = 0 ; a < 9 ; a++) {
+				if ((PINB & 0x0f) != 15) {
+					ckd = 0;
+					string2[14] = ' ';
+					string2[15] = ' ';
+					LCDInit();
+					break;
+				}
+				else if ((a > 5) || (a < 3)) {
+					bz(50, 200);
+				}
+				else {
+					bz(150, 200);
+				}
+			}
+			_delay_ms(200);
+		}
+		
+		
+		
+		// test display
+		static char string3[]="sonic";
+		static char string4[]="filter";
+		static char string5[]="elapse";
+		static char string6[]="motion";
+		static char string7[]="largest : ";
+		static char string9[]="second  : ";
+		
+		
+		unsigned char x[] = {7, 10, 13, 0, 3, 6, 9};
+		unsigned char y[] = {0, 0, 0, 1, 1, 1, 1};
+		
+		if ((PINB & 15) != 15) {
+			disp = (PINB & 15);
+			LCDInit();
+		}
 		LCDMove(0,0);
-		tNum(front[0]);
-		LCDMove(0,4);
-		tNum(front[1]);
-		LCDMove(0,8);
-		tNum(front[2]);
-		LCDMove(0,12);
-		tNum(front[3]);
 		
+		// 초음파 테스트
+		if(disp == 12) {
+			LCDPuts(string3);
+			for (char a = 0 ; a < 7 ; a++) {
+				LCDMove(y[a], x[a]);
+				tNum2(!((check >> a) & 0x01));
+			}
+			LCDMove(1,12);
+			tNum3((~check) & 0x7f);
+		}
+		else if(disp == 10) {
+			LCDPuts(string4);
+			for (char a = 0 ; a < 7 ; a++) {
+				LCDMove(y[a], x[a]);
+				tNum2(average[a]);
+			}
+			LCDMove(1,12);
+			tNum3((~check) & 0x7f);
+		}
+		else if(disp == 6) {
+			LCDPuts(string5);
+			for (char a = 0 ; a < 7 ; a++) {
+				LCDMove(y[a], x[a]);
+				tNum2(front[a]);
+			}
+			LCDMove(1,12);
+			tNum3((~check) & 0x7f);
+		}
+		else if(disp == 5) {
+			LCDMove(0,0);
+			LCDPuts(string7);
+			tNum3(largest);
+			
+			LCDMove(1,0);
+			LCDPuts(string9);
+			tNum3(secondLargest);
+		}
 		
-		LCDMove(1,0);
-		tNum(front[4]);
-		LCDMove(1,4);
-		tNum(front[5]);
-		LCDMove(1,8);
-		tNum(front[6]);
-		LCDMove(1,12);
-		tNum(check);	
+		// 움직임 감지 테스트
+		else if(disp == 9) {
+			LCDPuts(string6);
+			unsigned char aa = (PINE >> 4) | ((PIND & 0x0f) << 4);	
+			for (char a = 0 ; a < 6 ; a++) {
+				LCDMove(y[a], x[a]);
+				tNum2(!((aa >> a) & 0x01));
+			}
+			LCDMove(1, 9);
+			tNum2(!((aa >> 7) & 0x01));
+			LCDMove(1,12);
+			tNum3(aa);
+		}
+		
+		else {
+			disp = 14;
+			LCDPuts(string2);
+			Watch(1);
+		}
 	}
 }
-
-// 후면 인터럽트
-// 버저 sos
-
-// lcd 시간 + 위치
-
-// 낮 30초
-
-// 밤 10초
-
-// 감지되면 버저
-// 뭐 할지
